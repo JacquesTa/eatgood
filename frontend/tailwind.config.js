6ddc57d6ce5d7421dac310e8daa8844293e65ff3
jacques Mouafo Tagu <jacques_fabrice32@yahoo.fr>
2023-01-17 23:27:15 +0100

    Update to angular14 and installed tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#fefcfb",
+      whitish: "#FDFFFC",
+      blue: "#0496FF",
+      green: "#2a9d8f",
+      green2: "#588157",
+      green3: "#B3D2B2",
+      orange: "#FF9F1C",
+      rose: "#E71D36",
+      black: "#011627",
+    },
+    fontFamily: {
+      poppins: "Poppins",
+      shrikland: "Shrikland",
+      fasthand: "Fasthand",
+    },
+  },
+  plugins: [],
+};
